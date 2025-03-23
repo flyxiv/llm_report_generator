b87605ba9eb781e6cf5ff281eabcdce0a63ee025
flyxiv <78268298+flyxiv@users.noreply.github.com>
2025-03-23 23:43:54 +0900

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+_install_requires = [
+    'langchain',
+    'flask',
+    'pandas'
+]
+
+_package_excludes = [
+    '*.tests'
+]
+
+setup(
+    name='llm_report_generator',
+    version='0.0.1',
+    packages=find_packages(exclude=_package_excludes),
+    install_requires=_install_requires,
+    python_requires='>=3.11'
+)
